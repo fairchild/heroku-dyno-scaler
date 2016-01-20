4e36be40952b1c0440cc02ca61f6ba480d5b6d5e
fairchild <fairchild.michael@gmail.com>
2016-01-19 23:50:23 -0800

    add Readme and first iteration of scaling

---
+++ README.md
@@ -0,0 +1,22 @@
+Simple lambda function to help scale heroku dynos.
+The function receives SNS published events that contain a json
+message specifying the dyno type and desired quantity.
+
+    {type: 'worker', quantity: 1}
+
+This can be published from the rails app, or anywhere else that knows a worker will be needed.
+
+To develop:
+
+    npm install
+    node-lamba run
+
+`node-lambda run` will use the event.json as sample input to the lambda function and
+run it locally.
+
+To deploy, copy the `deploy.env.example` file to `deploy.env` and update its contents
+to match your needs and then
+
+    `node-lambda deploy`
+
+This will zip up the directory, including node_modules, upload the zip to AWS and make the deploy.env variables available on `process.env`
---
+++ index.js
@@ -0,0 +1,43 @@
+MAX_WORKERS = 3
+
+var HerokuClient = require('heroku-client');
+var heroku = new HerokuClient({ token: process.env.HEROKU_API_TOKEN });
+var app = heroku.apps(process.env.HEROKU_APP_NAME);
+
+exports.formationUpdate = function (desiredState, context) {
+  app.formation(desiredState.type)
+      .update({quantity: desiredState.quantity}, function(err, data) {
+      if (err) {
+        console.log('error scaling up', err);
+        context.fail
+        return err
+      }
+      console.log('scaled',desiredState.type, data);
+      context.succeed(data)
+    }
+  );
+}
+
+function parsedSNSmessage(event) {
+  return JSON.parse(event.Records[0].Sns.Message)
+}
+
+exports.handler = function(event, context) {
+  console.log(process.env.HEROKU_APP_NAME);
+  console.log(JSON.stringify(event));
+  var desiredState = parsedSNSmessage(event)
+  console.log('desiredState --> ', desiredState);
+  app.formation(desiredState.type).info(function(err, data) {
+    if (err) {
+      console.error('ERROR', err);
+      context.fail
+      return err;
+    }
+    console.log('there are currently '+ data.quantity +' '+ desiredState.type);
+    if (+data.quantity <= MAX_WORKERS && desiredState.quantity !== +data.quantity) {
+      this.formationUpdate(desiredState, context)
+    } else {
+      console.log('not scaling since there are already max workers or desired quantity reached');
+    }
+  });
+};
